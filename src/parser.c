b7e68547fc08ed880b4d9da4b2496d4ba191e368
daiki0321 <daiki@midokura.com>
2022-05-01 18:58:34 +0200

    Fixed 32bit environment in wasm

--- src/parser.c
+++ src/parser.c
@@ -1234,7 +1234,8 @@ void load_weights_upto(network *net, char *filename, int start, int cutoff)
     fread(&minor, sizeof(int), 1, fp);
     fread(&revision, sizeof(int), 1, fp);
     if ((major*10 + minor) >= 2 && major < 1000 && minor < 1000){
-        fread(net->seen, sizeof(size_t), 1, fp);
+        //fread(net->seen, sizeof(size_t), 1, fp);
+        fread(net->seen, 8, 1, fp);
     } else {
         int iseen = 0;
         fread(&iseen, sizeof(int), 1, fp);
